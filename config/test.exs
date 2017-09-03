237dd1677084d93c5c90649d9fb17204597ca996
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2017-09-03 17:09:38 -0400

    Fewer bcrypt iterations during tests

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,8 @@
 use Mix.Config
 
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rumbl, Rumbl.Endpoint,
